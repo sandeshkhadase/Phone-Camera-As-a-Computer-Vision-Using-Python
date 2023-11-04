388ada956ae591f06df2ca74c277769090efff16
Sandesh Khadase <sandeshkhadase05@gmail.com>
2023-11-04 21:57:53 +0530

    Add files via upload

---
+++ Phone-Camera-As-a-Computer-Vision-Using-Python.py
@@ -0,0 +1,17 @@
+#Innovation And Fun With @Sandeshkhadase
+#Innovation And Fun With @Sandeshkhadase
+#Innovation And Fun With @Sandeshkhadase
+
+
+import cv2
+import numpy as np
+url = "Your IP Address/video"
+cp = cv2.VideoCapture(url)
+while(True):
+    camera, frame = cap.read()
+    if frame is not None:
+        cv2.imshow("Frame", frame)
+    q = cv2.waitKey(1)
+    if q==ord("q"):
+        break
+cv2.destroyAllWindows()
